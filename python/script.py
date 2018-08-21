f14dc932ec4e131b65b8970978a0b70ad1425d54
Charles Fleury <chs.fleury@gmail.com>
2018-08-21 18:23:11 +0200

    add dockerfile and scripts

---
+++ python/script.py
@@ -0,0 +1,6 @@
+from java.lang import System
+
+s = 'Hello World'
+s = s + "2"
+System.out.println(s)
+print(s)
--- src/main/java/fr/chsfleury/jepfaas/Main.java
+++ src/main/java/fr/chsfleury/jepfaas/Main.java
@@ -4,18 +4,34 @@ import jep.Jep;
 import jep.JepException;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 @Slf4j
 public class Main {
 
-    public static void main(String[] args) {
-        try(Jep jep = new Jep()) {
-            jep.eval("from java.lang import System");
-            jep.eval("s = 'Hello World'");
-            jep.eval("System.out.println(s)");
-            jep.eval("print(s)");
-            jep.eval("print(s[1:-1])");
+    public static void main(String[] args) throws IOException {
+        try {
+            log.info("Starting jep");
+            Jep jep = new Jep();
+            Path p = Paths.get("/data/script.py");
+            try (Stream<String> lines = Files.lines(p)) {
+                lines.forEach(line -> eval(jep, line));
+            }
         } catch (JepException e) {
-            e.printStackTrace();
+            log.error("error", e);
+        }
+    }
+
+    private static void eval(Jep jep, String line) {
+        try {
+            log.info("eval {}", line);
+            jep.eval(line);
+        } catch (JepException e) {
+            log.error("error at: {}", line, e);
         }
     }
 
