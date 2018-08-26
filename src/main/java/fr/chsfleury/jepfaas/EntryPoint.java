41ff10b8bd016584455eb10b9b5447570a57f3ae
Charles Fleury <chs.fleury@gmail.com>
2018-08-26 18:40:14 +0200

    close the jep instance after eval + improve debug with python line

--- src/main/java/fr/chsfleury/jepfaas/EntryPoint.java
+++ src/main/java/fr/chsfleury/jepfaas/EntryPoint.java
@@ -22,16 +22,22 @@ public class EntryPoint {
 
         port(8081);
         post("/", (request, response) -> {
+            String evalLine = null;
             try {
                 Jep jep = new Jep();
-                jep.set("requestBody", request.body());
+//                jep.set("requestBody", request.body());
+                evalLine = "requestBody = \"" + request.body() + "\"";
+                jep.eval(evalLine);
                 for (String line : pythonScriptLines) {
-                    jep.eval(line);
+                    evalLine = line;
+                    jep.eval(evalLine);
                 }
-                return jep.getValue("responseBody");
+                Object responseBody = jep.getValue("responseBody");
+                jep.close();
+                return responseBody;
             } catch (JepException e) {
-                log.error("error", e);
-                return e.getMessage();
+                log.error("error at line: {}", evalLine, e);
+                return "error at line: '" + evalLine + "' => " + e.getMessage();
             }
         });
     }
